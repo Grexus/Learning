7bab5c14a2dc0eeab8747bf925f4363df04a0a7c
Unknown <matthew-grech@outlook.com>
2018-06-05 00:18:42 +0200

    Finished Section 2 - Lecture 12

---
+++ Udemy/The Complete Javascript Course/2-JS-Basics/script.js
@@ -0,0 +1,49 @@
+// Variables 1 - Different Variables
+
+// var name = "John"; // String
+// var lastName = "Smith"; // String
+// var age = "26"; // Number 
+// var fullAge = true; // Boolean
+
+// ---------------------------------------------------------------------//
+
+// Variables 2 - Prompts, Alerts & Type Coercion 
+
+// var fullName = prompt("Provide a random name");
+// var age = "23"; 
+// var fullAge = true; 
+// var job = "Web Designer"
+
+// alert(fullName + " is a " + age + ' year old ' + job);
+
+// ---------------------------------------------------------------------//
+
+// Operators 
+
+// var name = "John"
+// var age = 23;
+// var presentYear = 2018;
+// var birthYear = presentYear - age;
+
+// console.log(name + " was born in " + birthYear);
+
+
+// age++; // Increment age by 1
+
+// console.log(name + " will be " + age + " years old next year");
+
+// ---------------------------------------------------------------------//
+
+// If / Else Statements
+
+// var name = "John";
+// var age = 23;
+// var isMarried = false;
+
+// if (isMarried) {
+//     console.log(name + " is married.");
+// } else {
+//     console.log(name + " is forever alone.");
+// }
+
+// ---------------------------------------------------------------------//
