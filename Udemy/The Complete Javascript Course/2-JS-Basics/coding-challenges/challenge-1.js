bd35db528e7688f4c484d35c2b0106efb2f572f6
Unknown <matthew-grech@outlook.com>
2018-06-05 23:58:02 +0200

    Added question to coding challenge

--- Udemy/The Complete Javascript Course/2-JS-Basics/coding-challenges/challenge-1.js
+++ Udemy/The Complete Javascript Course/2-JS-Basics/coding-challenges/challenge-1.js
@@ -1,4 +1,16 @@
-// Change height & ages to test
+// Coding Challenge 1
+
+// John and a friend invented a simple game where the player with the highest value of his height (in centimeters) plus five times his age wins (what a silly game :)
+
+// 1. Create variables for the heights and ages of two friends and assign them some values
+// 2. Calculate their scores
+// 3. Decide who wins and print the winner to the console. Include the score in the string that you output to the console. Don't forget that there can be a draw (both players with the same score).
+
+// 4. EXTRA: Add a third player and now decide who wins. Hint: you will need the && operator to take the decision. If you can't solve this one, just watch the solution, it's no problem :)
+
+// -------------------------------------------------------------------------------- //
+
+// Solution
 
 // John
 var johnHeight = 176;
@@ -17,17 +29,17 @@ var tonyScore = tonyHeight + (tonyAge * 5);
 
 // Scoring
 if (johnScore > mikeScore && johnScore > tonyScore) {
-    console.log('John wins with a score of ' + johnScore + '!');
+    console.log("John wins with a score of " + johnScore + "!");
 } else if (mikeScore > johnScore && mikeScore > tonyScore ) {
-    console.log('Mike wins with a score of ' + mikeScore + '!');
+    console.log("Mike wins with a score of " + mikeScore + "!");
 } else if (tonyScore > johnScore && tonyScore > mikeScore) {
-    console.log('Tony wins with a score of ' + tonyScore + '!');
+    console.log("Tony wins with a score of " + tonyScore + "!");
 } else if (tonyScore > johnScore && tonyScore === mikeScore) {
-    console.log("It's tie between Tony & Mike with a score of " + tonyScore + '!');
+    console.log("It's tie between Tony & Mike with a score of " + tonyScore + "!");
 } else if (tonyScore > mikeScore && tonyScore === johnScore) {
-    console.log("It's tie between Tony & John with a score of " + tonyScore + '!');
+    console.log("It's tie between Tony & John with a score of " + tonyScore + "!");
 } else if (johnScore > tonyScore && johnScore === mikeScore) {
-    console.log("It's tie between John & Mike with a score of " + johnScore + '!');
+    console.log("It's tie between John & Mike with a score of " + johnScore + "!");
 } else {
     console.log("It's a threeway tie!");
 }
