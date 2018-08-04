da2dfbf1868ae4a47cd0d25678383b6fd0dc401e
Unknown <matthew-grech@outlook.com>
2018-08-04 12:10:15 +0200

    Finished Chapter 1

---
+++ Udemy/Complete C# Masterclass/Constants/Constants/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Constants
+{
+
+    // Constants are immutabe values which are known at compile time and do not change for the lifespan of the program
+
+    class Program
+    {
+
+        // Constants as fields
+        const int weeks = 52, months = 12;
+        const string birthday = "30th November 1994";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(birthday);
+            Console.Read();
+        }
+    }
+}
