f6f84c3845501f84625975109d6bfc95d2e70662
Unknown <matthew-grech@outlook.com>
2018-08-04 17:04:31 +0200

    Finished Chapter 3 Lecture 28

---
+++ Udemy/Complete C# Masterclass/Methods/MethodChallenge/MethodChallenge/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MethodChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string friend1 = "Janice";
+            string friend2 = "Karl";
+            string friend3 = "Ismael";
+
+            GreetFriend1(friend1);
+            GreetFriend1(friend2);
+            GreetFriend1(friend3);
+
+            GreetFriend2(friend1, friend2, friend3);
+
+            Console.Read();
+        }
+
+        public static void GreetFriend1(string friendName)
+        {
+            Console.WriteLine("Hello there " + friendName);
+        }
+
+        public static void GreetFriend2(string friendName1, string friendName2, string friendName3)
+        {
+            Console.WriteLine("Hello there " + friendName1);
+            Console.WriteLine("Hello there " + friendName2);
+            Console.WriteLine("Hello there " + friendName3);
+        }
+    }
+}
---
+++ Udemy/Complete C# Masterclass/Methods/ReturnMethods/ReturnMethods/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ReturnMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add(15, 22));
+            Console.WriteLine(Add(40, 22));
+            Console.WriteLine(Multiply(3, 10));
+            Console.WriteLine(Divide(9, 4));
+            Console.Read();
+        }
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public static double Divide(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
