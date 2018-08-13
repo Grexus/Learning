bfda1b0116b9c0a05258c6bc2e4a7203385b7c8e
Grexus <matthew-grech@outlook.com>
2018-08-13 19:52:31 +0200

    Finished Section 6 - Lecture 54

---
+++ Udemy/Complete C# Masterclass/Classes/ClassesBasics/ClassesBasics/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ClassesBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create an object of my class
+            // an instance of Human
+            Human matthew = new Human("Matthew", "Grech", "Brown", 23);
+            Human janice = new Human("Janice", "Grech", "Brown", 1);
+
+            matthew.Introduction();
+            janice.Introduction();
+
+            Console.Read();
+        }
+    }
+}
