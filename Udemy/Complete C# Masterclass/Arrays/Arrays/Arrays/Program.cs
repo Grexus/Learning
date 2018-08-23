b37e4dccb60bc5eb52093083092772e595d77fc6
Grexus <matthew-grech@outlook.com>
2018-08-23 19:31:11 +0200

    Finished Section 7 - Lecture 64

---
+++ Udemy/Complete C# Masterclass/Arrays/Arrays/Arrays/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] grades = new int[5];
+
+            grades[0] = 20;
+            grades[1] = 15;
+            grades[2] = 16;
+            grades[3] = 9;
+            grades[4] = 12;
+
+
+            Console.WriteLine("grades at index 0 : {0}", grades[0]);
+
+            // assign value to array grades at index 0
+            string input = Console.ReadLine();
+            grades[0] = int.Parse(input);
+            Console.WriteLine("grades at index 0 : {0}", grades[0]);
+
+
+            int[] gradesOfMathStudentsA = { 20, 13, 22, 15, 9 };
+            int[] gradesOfMathStudentsB = new int[] { 20, 13, 22, 15, 9 };
+
+            Console.WriteLine("Length of grades A is: {0}", gradesOfMathStudentsA.Length);
+            Console.Read();
+        }
+    }
+}
---
+++ Udemy/Complete C# Masterclass/Arrays/ForEachLoops/ForEachLoops/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ForEachLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[10];
+            
+            for(int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = i;
+                Console.WriteLine(nums[i]);
+            }
+
+            int counter = 0;
+            foreach(int j in nums)
+            {
+                Console.WriteLine("Element {0} = {1}", counter, j);
+                counter++;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Challenge Time!");
+            Console.WriteLine();
+
+            string[] friends = { "Janice", "Karl", "Sergio", "Ismael", "Macram"};
+            
+            foreach(string friend in friends)
+            {
+                Console.WriteLine("Greetings, {0}", friend);
+            }
+
+            Console.WriteLine("Challenge SUCCESS!");
+
+            Console.Read();
+        }
+    }
+}
