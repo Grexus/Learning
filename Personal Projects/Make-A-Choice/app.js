9b2c6519ffcfdc0f091f7f7ff789530e52ba6e1e
Grexus <matthew-grech@outlook.com>
2018-11-01 21:33:11 +0100

    added logic to pick a random item from array

--- Personal Projects/Make-A-Choice/app.js
+++ Personal Projects/Make-A-Choice/app.js
@@ -1,12 +1,15 @@
 let items = [];
 let userInput = document.getElementsByClassName("input")[0];
+let isDecided = false;
 
+// Event listener on user input to run 'getInput' function
 userInput.addEventListener('keydown', function(e){
     if (e.keyCode === 13) { // checks if 'enter' key was pressed
         getInput();
     }
 })
 
+// Get and Print input
 function getInput(){
     let itemList = document.getElementsByClassName('inputs')[0];
     let item = document.createElement('div');
@@ -21,4 +24,27 @@ function getInput(){
     
     // Empty input field
     userInput.value = "";
+}
+
+// Decide 
+function decide(arr){
+    if(items.length > 0 && !isDecided){
+        let decisionValue = Math.floor(Math.random() * items.length) + 1;
+        let decisionContainer = document.getElementsByClassName('choice')[0];
+        let decision = document.createElement('span');
+
+        decision.classList.add('final-decision');
+
+        decision.innerText = arr[decisionValue - 1];
+
+        decisionContainer.appendChild(decision);
+
+        isDecided = true;
+    }
+}
+
+// Reset - WIP
+function reset(){
+    items = [];
+    isDecided = false;
 }
